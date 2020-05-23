e41b4317da6ba1991300b1628f626d65b32b24be
christopher.ian.laine@gmail.com <christopher.ian.laine@gmail.com>
2020-05-23 12:58:02 +1200

    Removing this reference as its not part of this demo

--- AspNetCore.Conventions/Startup.cs
+++ AspNetCore.Conventions/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-[assembly: ApiConventionType(typeof(DefaultApiConventions))]
 namespace AspNetCore.WebConventions
 {
     public class Startup
